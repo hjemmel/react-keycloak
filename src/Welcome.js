54308ba2dbadd260371303dcaacb8d178f6fd9f2
Hans Jakob Emmel <hans.emmel@gmail.com>
2018-10-21 10:54:21 +1300

    first commit

---
+++ src/Welcome.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Welcome extends Component {
+    render() {
+        return (
+            <div className="Welcome">
+                <p>This is your public-facing component.</p>
+            </div>
+        );
+    }
+}
+export default Welcome;
